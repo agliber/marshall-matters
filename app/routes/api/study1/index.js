85f3b1b97370a76c89faa846ebef488407c269c3
Adam Liber <aliber@usc.edu>
2018-01-07 23:38:01 -0800

    minor fix all unchecked/checked case p1

--- app/routes/api/study1/index.js
+++ app/routes/api/study1/index.js
@@ -6,25 +6,29 @@ router.post("/", function(req,res,next){
     var con = req.app.get('connection');
     let user_id = req.surveySession.user_id;
     console.log(req.body);
-    if(Array.isArray(req.body.unchecked) ){
-      req.body.unchecked.forEach(function(characteristicNotChecked){
-
-          con.query(`UPDATE decision_making SET study1_${characteristicNotChecked} = 0 WHERE user_id = '${user_id}' ;`);
-      });
-    }else{
-      con.query(`UPDATE decision_making SET study1_${req.body.unchecked} = 0 WHERE user_id = '${user_id}' ;`);
+    if(req.body.unchecked){
+      if(Array.isArray(req.body.unchecked) ){
+        req.body.unchecked.forEach(function(characteristicNotChecked){
+            con.query(`UPDATE decision_making SET study1_${characteristicNotChecked} = 0 WHERE user_id = '${user_id}' ;`);
+        });
+      }else{
+        con.query(`UPDATE decision_making SET study1_${req.body.unchecked} = 0 WHERE user_id = '${user_id}' ;`);
+      }
     }
 
-    if(Array.isArray(req.body.characteristic) ){
-      req.body.characteristic.forEach(function(characteristic){
+    if(req.body.characteristic){
+      if(Array.isArray(req.body.characteristic) ){
+        req.body.characteristic.forEach(function(characteristic){
 
-          con.query(`UPDATE decision_making SET study1_${characteristic} = 1 WHERE user_id = '${user_id}' ;`);
+            con.query(`UPDATE decision_making SET study1_${characteristic} = 1 WHERE user_id = '${user_id}' ;`);
 
-      });
-    }else{
+        });
+      }else{
 
-      con.query(`UPDATE decision_making SET study1_${req.body.characteristic} = 1 WHERE user_id = '${user_id}' ;`);
+        con.query(`UPDATE decision_making SET study1_${req.body.characteristic} = 1 WHERE user_id = '${user_id}' ;`);
+      }
     }
+
 
     res.redirect('/study2P1.html');
     next();
